c4f6189582006bbbb1ba13fb0e4e4c455c9bbc36
valperez <valperez1214@gmail.com>
2021-05-13 16:13:23 -0600

    agregando lognormal

---
+++ functions/Model/lognormal.jl
@@ -0,0 +1,11 @@
+# Función para calcular una pregunta estilo "Cuál es tu edad?"
+# k es una matriz donde cada columna representa un centro de dist
+@model lognormal(k, alpha, beta, tau, mu, ::Type{T} = Float64) where {T} = begin
+    p          = Vector{T}(undef, size(k, 2))
+    m          = Vector{T}(undef, size(k, 2))
+    for i = 1:size(k, 2)
+         p[i] ~ Gamma(alpha, beta)
+         m[i] ~ Normal(mu, tau)
+         k[i] ~ LogNormal(m[i], p[i])
+    end
+end;
